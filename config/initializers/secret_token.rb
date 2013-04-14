ee9fc7c40a929858d0f4f3610707b460409dccd5
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2013-04-14 15:44:30 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mayushee::Application.config.secret_token = 'ddd801f42a2e0a413a7d317acb2a807554886a66bf76395c010e382c65ecc7ce0a9bbf3b17e2b8a7145e295183dcaf417134e7a230349e270e92ccadf62b1059'
