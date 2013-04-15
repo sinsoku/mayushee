7cb77686bace70ba8772858ddef26a44f8059e18
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2013-04-15 10:43:06 +0900

    Modify spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -19,6 +19,7 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe ProjectsController do
+  before { controller.stub(:authenticate_user!).and_return true }
 
   # This should return the minimal set of attributes required to create a valid
   # Project. As you add validations to Project, be sure to
