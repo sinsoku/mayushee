7c2e824bf56d61ddbb8f372b151a885df837bde8
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2013-04-20 11:49:47 +0900

    Fix a bug
    Change load_and_authorize_resource to authorize_resource

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,6 +1,8 @@
 class DutiesController < ApplicationController
   before_filter :authenticate_user!
 
+  authorize_resource
+
   def index
     @project = Project.find params[:project_id]
     @duties = @project.duties
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,14 @@
 
 FactoryGirl.define do
   factory :user do
+    login                 'yoshioka'
+    password              'password'
+    password_confirmation 'password'
+    name                  'Ryoji Yoshioka'
+    email                 'yoshioka.ryoji@gmail.com'
+    role                  'develop'
+    factory :admin_user do
+      role 'admin'
+    end
   end
 end
