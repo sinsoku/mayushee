415b5f8e113cf67c0b2b6def0bc0d07d4f1c5e8e
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2013-04-21 19:47:33 +0900

    Implement story create

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,33 @@
+class StoriesController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :set_project
+
+  authorize_resource
+
+  def index
+    @stories = @project.stories
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    if @project.stories.create(story_params)
+      redirect_to project_stories_url(@project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    def story_params
+      params.require(:story).
+        permit(:importance, :name, :description, :storypoints)
+    end
+end
